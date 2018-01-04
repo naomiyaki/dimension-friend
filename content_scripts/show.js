e95dc8d5d7af23c191545dc86a56f1597a57eb02
Naomi Rubin <naomi@castironcoding.com>
2018-01-04 12:38:18 -0800

    🐣  Run content script with logging

---
+++ content_scripts/show.js
@@ -0,0 +1,16 @@
+(function() {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  window.addEventListener('resize', function() {
+    console.log('Resizing!');
+  });
+
+})();
